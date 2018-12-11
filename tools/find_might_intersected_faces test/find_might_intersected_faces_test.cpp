ac96f757a3a335495c7122c36e1440c3f60b7fe2
jzxiong <309314775@qq.com>
2018-12-11 18:47:53 +0800

    add class might_intersected_faces_list to store faces need to be chect out

---
+++ tools/find_might_intersected_faces test/find_might_intersected_faces_test.cpp
@@ -0,0 +1,41 @@
+#include "octree.h"
+#include "stlreading.h"
+#include <iostream>
+
+int main()
+{
+	Region root_region_a = { 0,0,-0.1,8 };
+	Region root_region_b = { 1,1,-0.1,8 };
+	Octree tree_a(0, root_region_a, 0.1, 5);
+	Octree tree_b(0, root_region_b, 0.1, 5);
+
+	for (int j = 0; j < 8; j++)
+	{
+		Vector3f normal(0, 0, 1);
+		Vector3f vertex[3];
+		float i = j;
+		float k = i + 0.5;
+		vertex[0] = { i,i,0 };
+		vertex[1] = { i,k,0 };
+		vertex[2] = { k,i,0 };
+		EleFace *tmpEle=new EleFace(normal, vertex[0], vertex[1], vertex[2]);
+		tree_a.insertEle(tmpEle);
+	}
+
+	for (int j = 1; j < 9; j++)
+	{
+		Vector3f normal(0, 0, 1);
+		Vector3f vertex[3];
+		float i = j;
+		float k = i + 0.5;
+		vertex[0] = { i,i,0 };
+		vertex[1] = { i,k,0 };
+		vertex[2] = { k,i,0 };
+		EleFace *tmpEle = new EleFace(normal, vertex[0], vertex[1], vertex[2]);
+		tree_b.insertEle(tmpEle);
+	}
+	
+	might_intersected_faces_list a_b(&tree_a, &tree_b);
+	
+	return 0;
+}
